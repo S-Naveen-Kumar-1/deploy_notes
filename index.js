07738ac25d24e0940a7333ebe1558082ab6ba840
S NAVEEN KUMAR <navee1110@gmail.com>
2023-10-26 22:36:07 +0530

    cors community middleware added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const {connection} = require("./db");
 const { userRouter } = require("./routes/user.routes");
 const { noteRouter } = require("./routes/notes.routes");
+const cors=require("cors")
 const app = express();
 app.use(express.json());
+app.use(cors())
 require('dotenv').config()
 
 app.use("/users",userRouter)
